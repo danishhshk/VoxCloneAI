cea9301aa3ab540c95c5c7d9c21cb748969d6272
Danish Shaikh <shaikhdanishpc@gmail.com>
2025-12-30 12:49:37 +0530

    3

--- server/src/app.js
+++ server/src/app.js
@@ -16,6 +16,8 @@ const app = express();
 // Middleware
 app.use(cors({
   origin: process.env.FRONTEND_URL,
+  methods: ["GET", "POST", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true
 }));
 app.use(express.json());
